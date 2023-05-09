afb720b699f7fcc7b2a5ddaff27bbae1abfce7ad
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-09 14:49:37 +0200

    prove di push

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import HeaderComp from './components/HeaderComp.vue';
 import MainComp from './components/MainComp.vue';
 import {store} from './store';
-import axios from 'axios'
+import axios from 'axios';
 
 
 
